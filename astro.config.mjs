9ae8e2fd9568068c9745c08dfd22e2e69377f07f
Anav Chand <anavkki@gmail.com>
2023-06-02 18:23:38 +1200

    E

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  staticPaths: {
-    '/admin': 'static/admin',
-  },
+  integrations: [tailwind()]
 });
